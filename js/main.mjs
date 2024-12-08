07ed010553211ac2a8a1750fea8cc433943fcadc
Sergey <gruzdosergey@gmail.com>
2024-12-08 15:55:32 +0500

    Дополнительное задание в functions.js

--- js/main.mjs
+++ js/main.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import {generateComments} from './comments.mjs';
 import {generatePhotos} from './photos.mjs';
 import { renderPictures } from './PictureRenderer.js';
