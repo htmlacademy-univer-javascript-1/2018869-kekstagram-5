868f6f26980f688f1156a0ecaa8daedfc72149bd
Sergey <gruzdosergey@gmail.com>
2025-01-04 22:52:08 +0500

    финал

--- js/form-handler.js
+++ js/form-handler.js
@@ -2,7 +2,7 @@ import { showAlert, successMessage, isEscapeKey } from './utils.js';
 import { sendData } from './api.js';
 import { pristine } from './validation.js';
 import { createPicture } from './render.js';
-// бессмысленный коммент 2
+
 
 const form = document.querySelector('.img-upload__form');
 const uploadFile = form.querySelector('#upload-file');
