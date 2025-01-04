fec78cc0044cd01c363edc98037a8bc3e7954c15
Sergey <gruzdosergey@gmail.com>
2025-01-04 21:51:27 +0500

    ФИНАЛ

---
+++ js/main.js
@@ -0,0 +1,12 @@
+import { renderPictures } from './render.js';
+import { getData } from './api.js';
+import './form-handler.js';
+import './validation.js';
+import './pictures.js';
+import './sorting.js';
+import { turnFilterOn, filterPictures } from './sorting.js';
+
+getData((posts) => {
+  turnFilterOn(posts);
+  renderPictures(filterPictures());
+});
---
+++ js/utils.js
@@ -0,0 +1,71 @@
+const successTemplate = document.querySelector('#success').content.querySelector('.success');
+const successButton = successTemplate.querySelector('.success__button');
+const ALERT_SHOW_TIME = 5000;
+
+function getRandomPositiveInteger (a, b = 0) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+function createIdGenerator() {
+  let lastGeneratedId = 0;
+  return function() {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.top = 0;
+  alertContainer.style.left = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 5px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const successMessage = () => {
+  const fragment = document.createDocumentFragment();
+  fragment.append(successTemplate);
+  document.body.append(fragment);
+};
+
+const onSuccessButtonCLick = () => {
+  document.querySelector('.success').remove();
+};
+
+successButton.addEventListener('click', onSuccessButtonCLick);
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export { getRandomPositiveInteger,
+  getRandomArrayElement,
+  createIdGenerator,
+  isEscapeKey,
+  showAlert,
+  successMessage,
+  debounce };
